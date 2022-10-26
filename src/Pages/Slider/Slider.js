4e534730ab74877659447b64b35f608b475fd0fb
sumaya-kabir <sumayakabir4547@gmail.com>
2022-10-26 12:06:55 +0600

    carousel set

--- src/Pages/Slider/Slider.js
+++ src/Pages/Slider/Slider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import banner1 from '../../assets/images/banner1.png';
-import banner2 from '../../assets/images/banner2.png';
+import banner2 from '../../assets/images/banner2 (3).png';
 
 
 const Slider = () => {
@@ -9,6 +9,7 @@ const Slider = () => {
         <Carousel fade>
       <Carousel.Item>
         <img
+          
           className="img-fluid d-block w-100"
           src={banner1}
           alt="First slide"
